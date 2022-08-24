306d5ec2ffe8b9b48e927c0b97c0b9e5b58941e2
rbkretzer <rafael.bkretzer@gmail.com>
2022-08-24 17:48:46 -0300

    Adicionado imagens e iniciado alterações relacionads aos eventos

--- src/main/java/com/equipeglr/App.java
+++ src/main/java/com/equipeglr/App.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
@@ -22,47 +23,10 @@ import java.lang.reflect.InvocationTargetException;
 public class App extends Application {
 
     private static Scene scene;
-
-    private Parent createContent() {
-        TextArea area = new TextArea();
-        area.setWrapText(true);
-        area.setOnKeyPressed(event -> {
-            if (event.getCode().equals(KeyCode.ENTER)) {
-                System.out.println("Apertei enter");
-                System.out.println("Adicionando linha: " + area.getParagraphs().size());
-            }
-            if (event.getCode().equals(KeyCode.BACK_SPACE) || event.getCode().equals(KeyCode.DELETE)) {
-                System.out.println("Removendo linha: " + area.getParagraphs().size());
-            }
-
-        });
-
-        Button append = new Button("append paragraph");
-        append.setOnAction(e -> {
-            area.appendText("Adicionando texto"+"\n ");
-            System.out.println("paragraphs: " + area.getParagraphs().size());
-        });
-        Button logLines = new Button("log lines");
-        logLines.setOnAction(e -> {
-            Text text = (Text) area.lookup(".text");
-            // getTextLayout is a private method in text, have to access reflectively
-            // this is my utility method, use your own :)
-            TextLayout layout;
-            try {
-                layout = (TextLayout) FXUtils.invokeGetMethodValue(Text.class, text, "getTextLayout");
-            } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException ex) {
-                throw new RuntimeException(ex);
-            }
-            System.out.println(layout.getLines().length);
-        });
-        BorderPane content = new BorderPane(area);
-        content.setBottom(new HBox(10, append, logLines));
-        return content;
-    }
-
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        Parent fxml = loadFXML("sample2");
+        scene = new Scene(fxml, 640, 480);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/equipeglr/Sample2.java
+++ src/main/java/com/equipeglr/Sample2.java
@@ -2,22 +2,28 @@ package com.equipeglr;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ZoomEvent;
+import javafx.scene.text.Font;
 
 import java.io.IOException;
 
 public class Sample2 {
 
-    public Label btnNovo;
-    public Label btnAbrir;
-    public Label btnSalvar;
-    public Label btnCopiar;
-    public Label btnColar;
-    public Label btnRecortar;
-    public Label btnCompilar;
-    public Label btnEquipe;
-    public TextArea areaCodigo;
+    @FXML private Label btnNovo;
+    @FXML private Label btnAbrir;
+    @FXML private Label btnSalvar;
+    @FXML private Label btnCopiar;
+    @FXML private Label btnColar;
+    @FXML private Label btnRecortar;
+    @FXML private Label btnCompilar;
+    @FXML private Label btnEquipe;
+    @FXML private TextArea areaCodigo;
+    @FXML private ListView linhas;
 
     public void colarTexto(MouseEvent mouseEvent) {
     }
@@ -41,5 +47,25 @@ public class Sample2 {
     }
 
     public void copiarTexto(MouseEvent mouseEvent) {
+    }
+
+    public void alteraContadorDeLinha(KeyEvent keyEvent) {
+
+        if (keyEvent.getCode().equals(KeyCode.ENTER)) {
+            System.out.println("Apertei enter");
+            System.out.println("Adicionando linha: " + areaCodigo.getParagraphs().size());
+            linhas.getItems().add(areaCodigo.getParagraphs().size());
+        }
+        if (keyEvent.getCode().equals(KeyCode.BACK_SPACE) || keyEvent.getCode().equals(KeyCode.DELETE)) {
+            if (areaCodigo.getParagraphs().size() != linhas.getItems().size()) {
+                System.out.println("Removendo linha: " + areaCodigo.getParagraphs().size());
+                linhas.getItems().remove(areaCodigo.getParagraphs().size());
+            }
+        }
+    }
+
+    public void aumentarFonteCodigo(ZoomEvent zoomEvent) {
+        Font novaFonte = new Font(areaCodigo.getFont().getSize());
+        areaCodigo.setFont(novaFonte);
     }
 }
