df2fd5847a853c8137ba5ed6bb95f16c0693d74d
rbkretzer <rafael.bkretzer@gmail.com>
2022-08-28 22:47:40 -0300

    implementada ações dos botões e realizadas melhorias

--- src/main/java/com/equipeglr/App.java
+++ src/main/java/com/equipeglr/App.java
@@ -4,30 +4,30 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
 
-    private static Scene scene;
     @Override
     public void start(Stage stage) throws IOException {
-        Parent fxml = loadFXML("sample2");
-        scene = new Scene(fxml, 910, 600);
+        Scene scene = new Scene(loadFXML("tela"), 910, 600);
         stage.setScene(scene);
         stage.setFullScreen(false);
         stage.minHeightProperty().set(600);
         stage.minWidthProperty().set(910);
-
+        //InputStream img = new BufferedInputStream(new FileInputStream("terminal.png"));
+        //stage.getIcons().add(new Image(img));
+        stage.setTitle("Compilador GLR");
         stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
---
+++ src/main/java/com/equipeglr/TelaController.java
@@ -0,0 +1,129 @@
+package com.equipeglr;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.FileChooser;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class TelaController {
+
+    @FXML private Label labelStatus;
+    @FXML private TextArea areaMensagem;
+    @FXML private Label btnNovo;
+    @FXML private Label btnAbrir;
+    @FXML private Label btnSalvar;
+    @FXML private Label btnCopiar;
+    @FXML private Label btnColar;
+    @FXML private Label btnRecortar;
+    @FXML private Label btnCompilar;
+    @FXML private Label btnEquipe;
+    @FXML private TextArea areaCodigo;
+    @FXML private ListView<Integer> linhas;
+
+    private static FileChooser ufc = new FileChooser();
+
+    public void initialize() {
+        linhas.getItems().add(areaCodigo.getParagraphs().size());
+        ufc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+    }
+
+    public void colarTexto() {
+        areaCodigo.paste();
+        adicionarLinhas();
+    }
+
+    public void recortarTexto() {
+        areaCodigo.cut();
+        removerLinhas();
+    }
+
+    public void compilarArquivo() {
+        areaMensagem.setText("->\tcompilação de programas ainda não foi implementada");
+    }
+
+    public void mostrarEquipe() {
+        areaMensagem.setText("->\tGabriel Adriano Rodrigues\n->\tLeonardo Souza Nunes\n->\tRafael Barbosa Kretzer");
+    }
+
+    public void abrirNovo() {
+        labelStatus.setText("");
+        areaCodigo.setText("");
+        areaMensagem.setText("");
+        removerLinhas();
+    }
+
+    public void abrirArquivo() throws IOException {
+        File selectedFile = ufc.showOpenDialog(btnAbrir.getScene().getWindow());
+        if (selectedFile != null) {
+            String s = Files.readString(selectedFile.toPath(), StandardCharsets.UTF_8);
+            areaCodigo.setText(s);
+            labelStatus.setText(selectedFile.getAbsolutePath());
+            adicionarLinhas();
+        }
+    }
+
+    private void adicionarLinhas() {
+        int linhasExistentes = linhas.getItems().size();
+        IntStream.range(0, areaCodigo.getParagraphs().size()).forEach(x -> {
+            int linhaAtual = x+1;
+            if (linhaAtual > linhasExistentes) {
+                linhas.getItems().add(linhaAtual);
+            }
+        });
+    }
+
+    private void removerLinhas() {
+        int linhasExistentes = areaCodigo.getParagraphs().size();
+        List<Integer> linhaParaRemover = linhas.getItems().stream().filter(l -> l > linhasExistentes).collect(Collectors.toList());
+        linhaParaRemover.forEach(l -> linhas.getItems().remove(l));
+    }
+
+    public void salvarArquivo() throws IOException {
+        if (labelStatus.getText() == null || labelStatus.getText().isEmpty()) {
+            criarNovo();
+        }
+        if (labelStatus.getText() != null || !labelStatus.getText().isEmpty()) {
+            FileWriter writer = new FileWriter(labelStatus.getText(), Charset.availableCharsets().get("UTF-8"), false);
+            writer.write(areaCodigo.getText());
+            writer.close();
+        }
+    }
+
+    private void criarNovo() throws IOException {
+        File arquivo = ufc.showSaveDialog(areaCodigo.getScene().getWindow());
+        if (arquivo == null){
+            return;
+        }
+        arquivo.createNewFile();
+        labelStatus.setText(arquivo.getAbsolutePath());
+    }
+
+    public void copiarTexto() {
+        areaCodigo.copy();
+    }
+
+    public void alterarContadorDeLinhaAoDigitar(KeyEvent keyEvent) throws IOException {
+        if (keyEvent.getCode().equals(KeyCode.ENTER) || (keyEvent.getCode().equals(KeyCode.V) && keyEvent.isControlDown())) {
+            adicionarLinhas();
+        }
+        if (keyEvent.getCode().equals(KeyCode.BACK_SPACE) || keyEvent.getCode().equals(KeyCode.DELETE) || (keyEvent.getCode().equals(KeyCode.X) && keyEvent.isControlDown())) {
+            if (areaCodigo.getParagraphs().size() != linhas.getItems().size()) {
+                removerLinhas();
+            }
+        }
+        if (keyEvent.isControlDown() && keyEvent.getCode().equals(KeyCode.S)) {
+            salvarArquivo();
+        }
+    }
+}
